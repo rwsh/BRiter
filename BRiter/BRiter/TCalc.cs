f7a554a643b19dba3873c8fb2f87ded4e6914e06
Roman Shamin <roman@shamin.ru>
2018-11-19 10:07:35 +0300

    Add files via upload

---
+++ BRiter/BRiter/TCalc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRiter
+{
+    static class Calc
+    {
+        public static Random rnd;
+
+        public static void Init()
+        {
+            rnd = new Random();
+        }
+
+        public static double S2(double x)
+        {
+            return x * x;
+        }
+
+        public static double U(double a, double b)
+        {
+            return a + (b - a) * rnd.NextDouble();
+        }
+
+        public static bool B()
+        {
+            if (rnd.Next(2) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static double Exp(double la)
+        {
+            return (-1.0 / la) * Math.Log(U(1e-17, 1));
+        }
+
+        public static double N(double m, double sigma)
+        {
+            double r = U(0, 1);
+            double phi = U(0, 1);
+
+            double res = Math.Cos(2 * Math.PI * phi) * Math.Sqrt(-2 * Math.Log(r));
+
+            res = sigma * res + m;
+
+            return res;
+        }
+
+    }
+}
---
+++ BRiter/BRiter/TGame.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRiter
+{
+    class TGame
+    {
+        int N;
+        int L;
+
+        double[] X, Y;
+        int[] iX, iY;
+
+        double[,] A;
+
+        double v_max = 0;
+        double v_min = 0;
+
+        public TGame()
+        {
+            Calc.Init();
+
+            L = 1000000;
+            N = 3;
+
+            X = new double[N];
+            Y = new double[N];
+
+            iX = new int[N];
+            iY = new int[N];
+
+            X[0] = 1;
+            X[1] = 2;
+            X[2] = 3;
+
+            Y[0] = 1;
+            Y[1] = 2;
+            Y[2] = 3;
+
+            FormA();
+
+            Run();
+
+            for (int n = 0; n < N; n++)
+            {
+                X[n] = iX[n] / (double)L;
+                Y[n] = iY[n] / (double)L;
+            }
+
+            v_max /= L;
+            v_min /= L;
+
+            Console.WriteLine("V = {0}\t{1}", v_max, v_min);
+
+            for (int n = 0; n < N; n++)
+            {
+                Console.Write("{0}\t", X[n]);
+            }
+            Console.WriteLine("\n");
+
+            for (int n = 0; n < N; n++)
+            {
+                Console.Write("{0}\t", Y[n]);
+            }
+            Console.WriteLine("\n");
+
+        }
+
+        void Run()
+        {
+            iX[0] = 1;
+            iY[0] = 1;
+
+            for (int l = 0; l < L; l++)
+            {
+                double max = double.MinValue;
+                int max_i = 0;
+
+                for (int i = 0; i < N; i++)
+                {
+                    if (ForMax(i) > max)
+                    {
+                        max = ForMax(i);
+                        max_i = i;
+                    }
+                }
+
+                double min = double.MaxValue;
+                int min_j = 0;
+
+                for (int j = 0; j < N; j++)
+                {
+                    if (ForMin(j) < min)
+                    {
+                        min = ForMin(j);
+                        min_j = j;
+                    }
+                }
+
+                iX[max_i]++;
+                iY[min_j]++;
+
+                v_max = max;
+                v_min = min;
+            }
+        }
+
+        double ForMax(int i)
+        {
+            double Res = 0;
+
+            for (int j = 0; j < N; j++)
+            {
+                Res += A[i, j] * iY[j];
+            }
+
+            return Res;
+        }
+
+        double ForMin(int j)
+        {
+            double Res = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                Res += A[i, j] * iX[i];
+            }
+
+            return Res;
+        }
+
+        void FormA()
+        {
+            A = new double[N, N];
+
+            Console.WriteLine("A\n");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = X[i] - Y[j] + Calc.N(0, 3);
+
+                    Console.Write("{0}\t", A[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
